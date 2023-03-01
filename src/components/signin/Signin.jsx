ba6db58532da3439fdbe9c9b40552f6515c48e89
Geradot <toshkageradot@tuta.io>
2023-03-01 15:32:44 +0400

    Pull and change

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import classes from "./Signin.module.css";
 import clsx from 'clsx';
