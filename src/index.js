2d30297210365352addbb43c53613e785b736a42
Geradot <toshkageradot@tuta.io>
2023-03-22 17:22:40 +0400

    Мелкие изменения

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import App from "./components/App/App";
 import ErrorBoundary from "./ErrorBoundary";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ErrorBoundary>
-    <App />
+    <BrowserRouter basename="/prometheus-x-course-task">
+      <App />
+    </BrowserRouter>
   </ErrorBoundary>
 );
